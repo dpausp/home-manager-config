c20de69c6f5b04ec8099d7256b2e2011c85bce10
Tobias dpausp <dpausp@posteo.de>
2022-06-04 14:50:53 +0200

    NixOS: typing-optimized systemd aliases
    Should be easy to type and edit with Neo2 layout.

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -7,10 +7,18 @@
     };
 
     shellAliases = {
-      # systemd
-      sc = "systemctl";
-      jc = "journalctl";
-      usc = "systemctl --user";
+      # I use systemctl and journalctl all the time, I need convenient aliases :)
+      start = "sudo systemctl start";
+      stop = "sudo systemctl stop";
+      status = "sudo systemctl status";
+      restart = "sudo systemctl restart";
+      # >S<ystemctl
+      s = "sudo systemctl";
+      # j>O<urnalctl
+      # o is easier to reach than j with Neo2 layout
+      o = "journalctl";
+      j = "journalctl";
+      us = "systemctl --user";
 
       # other
       m = "mount";
