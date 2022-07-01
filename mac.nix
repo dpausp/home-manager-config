56d65767640ade38ba6bcc9cfc2a680aab4c0cb7
Tobias dpausp <dpausp@posteo.de>
2022-07-01 23:43:12 +0200

    Initial config for the Mac, mostly from old dotfiles

---
+++ mac.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, lib, ... }:
+
+with builtins;
+
+{
+
+  imports = [
+    modules/base-common.nix
+    modules/graphical.nix
+    modules/nixdev.nix
+    modules/pythondev.nix
+    modules/zsh.nix
+  ];
+
+  home.packages = with pkgs; [
+    cachix
+    cacert
+    dmenu
+    git
+    gitAndTools.git-annex
+    img2pdf
+    jetbrains.pycharm-community
+    kubectl
+    ngrok
+    nix
+    pandoc
+    pgcli
+    sqlitebrowser
+    sshfs-fuse
+    unison
+  ];
+
+  nixpkgs.config = {
+    allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [ "ngrok" ];
+  };
+
+  programs.feh.enable = true;
+  programs.zathura.enable = true;
+
+  programs.zsh = {
+
+    initExtra = ''
+      function git-copy-commit-msg {
+        commit=''${1:-HEAD}
+        git log --format=%B -n1 $commit | pbcopy
+      }
+
+      function git-copy-commit-id {
+        commit=''${1:-HEAD}
+        git rev-parse $commit | tr -d '\n' | pbcopy
+      }
+
+      unalias ls
+      bindkey -M viins '\e\C-h' backward-kill-word
+    '';
+
+    shellAliases = {
+      fc-copy-nixpkgs-version = "jq -r '.nixpkgs.rev' < versions.json | tr -d '\n' | pbcopy";
+    };
+
+    shellGlobalAliases = {
+      X = "| tr -d '\n' | pbcopy";
+    };
+  };
+}
+
