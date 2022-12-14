9decbbc1848c8dff99ac778230b5b78e318dc115
Tobias dpausp <dpausp@posteo.de>
2022-12-14 10:03:25 +0000

    update flake template for 22.11

--- template-nixos-host-flake.nix
+++ template-nixos-host-flake.nix
@@ -1,20 +1,36 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hm-config.url = "github:dpausp/home-manager-config";
+    #hm-config.url = "path:./home-manager-config";
+    #hm-config.url = "github:dpausp/home-manager-config";
   };
 
   outputs = inputs: {
-    homeConfigurations = {
+    homeConfigurations = 
+    let
+      system = "x86_64-linux";
+      homeDirectory = "/home/ts";
+      username = "ts";
+      stateVersion = "22.11";
+      inherit (inputs) hm-config nixpkgs nixpkgs-unstable;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
       ts = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        homeDirectory = "/home/ts";
-        username = "ts";
-        stateVersion = "22.05";
-        configuration.imports = [ 
-          "${inputs.hm-config}/host.nix"
+        extraSpecialArgs = {
+          inherit nixpkgs nixpkgs-unstable;
+        };
+        inherit pkgs;
+        modules = [
+          "${hm-config}/host.nix"
+          {
+            home = {
+              inherit username homeDirectory stateVersion;
+            };
+          }
         ];
       };
     };
