986cc9aab55e1d351642a8302c9325d0572af4b8
Tobias dpausp <dpausp@posteo.de>
2022-12-29 20:58:24 +0100

    pythondev: fix blackd

--- modules/pythondev.nix
+++ modules/pythondev.nix
@@ -6,7 +6,9 @@ with builtins;
   home.packages = with pkgs.python310Packages; [
     pkgs.python39
     (pkgs.python310.overrideAttrs (_: { meta.priority = 3; }) )
-    black
+    (black.overridePythonAttrs (_: {
+      propagatedBuildInputs = black.propagatedBuildInputs ++ black.optional-dependencies.d;
+    }))
     isort
     pkgs.pre-commit
     pip
