307494c2f78ee4703e10b2bcabc73ea9d6620173
Tobias dpausp <dpausp@posteo.de>
2024-08-22 20:44:19 +0200

    Eython 3.11 as default, add 3.12

--- modules/pythondev.nix
+++ modules/pythondev.nix
@@ -3,10 +3,11 @@
 with builtins;
 
 {
-  home.packages = with pkgs.python310Packages; [
+  home.packages = with pkgs.python3Packages; [
     pkgs.python39
     (pkgs.python310.overrideAttrs (_: { meta.priority = 4; }) )
     (pkgs.python311.overrideAttrs (_: { meta.priority = 3; }) )
+    (pkgs.python312.overrideAttrs (_: { meta.priority = 4; }) )
     (black.overridePythonAttrs (_: {
       propagatedBuildInputs = black.propagatedBuildInputs ++ black.optional-dependencies.d;
     }))
