6e9180e31e58181aca428445a834f7baae30d83c
Tobias dpausp <dpausp@posteo.de>
2025-08-06 16:46:22 +0200

    wip hm for ki agents

---
+++ mac_kiagents.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, pkgs-unstable, devenv, lib, ... }:
+
+with builtins;
+
+{
+  imports = [
+    modules/ai.nix
+    modules/base-common.nix
+    modules/dev.nix
+    modules/mac.nix
+    modules/neovim.nix
+    modules/nixdev.nix
+    modules/pin-flakes.nix
+    modules/pythondev.nix
+    modules/zellij.nix
+    modules/zsh.nix
+  ];
+
+  home = {
+    username = "rovodev";
+    stateVersion = "25.05";
+    homeDirectory = "/Users/rovodev";
+  };
+
+  nixpkgs.overlays = [
+    (self: super: {
+    })
+  ];
+
+  programs.zsh = {
+  };
+}
+
